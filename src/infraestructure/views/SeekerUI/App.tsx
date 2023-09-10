3a95592b6b81e900158280c6dda27f3b68687da5
stephane <fs@email.com>
2023-09-10 17:38:30 +0200

    feat: vscode webview ui toolkit

--- src/infraestructure/views/SeekerUI/App.tsx
+++ src/infraestructure/views/SeekerUI/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { environment } from "../../../environment-config";
+import { VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 //import "./App.css";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -8,6 +9,7 @@ export const App = () => {
     <>
       <h1>baby</h1>
       <input></input>
+      <VSCodeTextField>Text Field Label</VSCodeTextField>
       {environment.dbScriptPath}
     </>
   );
