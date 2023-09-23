05ccca71abf69c049fcd8c1a6f9629b60af690f8
stephane <fs@email.com>
2023-09-23 18:52:22 +0200

    Squashed commit of the following:
    commit 7995524b77b7b086c4374a4417763f782f39de9a
    Author: stephane <fs@email.com>
    Date:   Sat Sep 23 18:51:43 2023 +0200
    
        fix: css-loader fixed

--- src/infraestructure/views/SeekerUI/index.tsx
+++ src/infraestructure/views/SeekerUI/index.tsx
@@ -1,10 +1,9 @@
-import React = require("react");
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import React = require('react');
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-const root = createRoot(document.getElementById("seekerSideBar")!);
+const root = createRoot(document.getElementById('seekerSideBar')!);
 
-/* TODO: keep researching how menus works  */
 root.render(
   <>
     <React.StrictMode>
