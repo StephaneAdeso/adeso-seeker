e311589cd0d8dc27fa9f32e38de7da680421a99c
stephane <fs@email.com>
2023-09-23 15:06:31 +0200

    feat: refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { SeekerSidebarViewProvider } from './infraestructure/providers/seeker-sidebar.view-provider';
-import { PersistanceService } from './infraestructure/persistence/persistence.service';
 import { newCollection, newRequest } from './infraestructure/commands';
+import { PersistanceService } from './infraestructure/persistence/persistence.service';
+import { SeekerSidebarViewProvider } from './infraestructure/providers/seeker-sidebar.view-provider';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // init data base
   PersistanceService.getInstance().initService(context);
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-
-  context.subscriptions.push(...registerCommands());
-
-  // sidebarViews
-  const sidebarProvider = new SeekerSidebarViewProvider(context.extensionUri);
-  vscode.window.registerWebviewViewProvider(
-    SeekerSidebarViewProvider.viewType,
-    sidebarProvider
-  );
+  context.subscriptions.push(...registerCommands(), ...registerViews(context));
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
 
+/* ----------------------------------------------
+|                  COMMANDS                     |
+---------------------------------------------- */
 const registerCommands = () => {
-  const disposables: vscode.Disposable[] = [];
+  const commandDisposables: vscode.Disposable[] = [];
 
-  disposables.push(
+  commandDisposables.push(
     vscode.commands.registerCommand('adeso-seeker.new-request', newRequest),
     vscode.commands.registerCommand(
       'adeso-seeker.new-collection',
@@ -39,5 +26,21 @@ const registerCommands = () => {
     )
   );
 
-  return disposables;
+  return commandDisposables;
+};
+
+/* ----------------------------------------------
+|                     VIEWS                     |
+---------------------------------------------- */
+const registerViews = (context: vscode.ExtensionContext) => {
+  const viewsDisposables: vscode.Disposable[] = [];
+
+  viewsDisposables.push(
+    vscode.window.registerWebviewViewProvider(
+      SeekerSidebarViewProvider.viewType,
+      new SeekerSidebarViewProvider(context.extensionUri)
+    )
+  );
+
+  return viewsDisposables;
 };
